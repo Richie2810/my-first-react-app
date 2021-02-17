7a0769a2eeac9e6554fa7c7ba657c2f2ea89b180
Richie2810 <richiegoneill@gmail.com>
2021-02-17 15:34:05 +0100

    Welcome to Codaisseur

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to my react site.
+          Welcome to Codaisseur
         </p>
         <a
           className="App-link"
